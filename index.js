40638f001e01ac88b37b39975671b2440f2ae14d
Joe Duncko <JoeDuncko@users.noreply.github.com>
2020-04-19 15:01:08 -0400

    Cleanup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
 const chunks = require('array.chunk');
 
-const token = "BQBpwO5cJhRLqoa3b1CWfi3iAe_uZvOVXfkhsJbrJ2HZyEm3-EFJzAJkrXu4L67AfVOAMiAEj1-d7g3hv4LP77W_pv0ZyWDSu77D-ucTftWkaKOPeFV7XAs8k8iFAfFSQNdXay4uKiSApJitlwTG3mcAuvWWTwRqAOtNmBXaQzL0xqOLRE1pCdOOZpaJhml73S8"
+const token = "BQAN1JGZ7VgFGyZkXToVRDLItqRSh_Tx7tGcJxISBeAjdlZUdiEkQrqhTN01vM6kGN7ZUQXAJysuJyw9jQGJccipAh6ntJ6f7Achf2lhV2C8BJ6YA5PKX0unHVhxa-_JHg0qd36zlGkrghNXELub2ma3zPaj6yMJ7g6rIw75xYKrD3j0PLXe_t9eTg-QIen_yls"
 const bearer = 'Bearer ' + token;
 
-async function deleteItem(itemIds) {
+async function deleteTrack(itemIds) {
     try {
         const response = await fetch(
             `https://api.spotify.com/v1/me/tracks?ids=${itemIds}`,
@@ -17,49 +17,59 @@ async function deleteItem(itemIds) {
         );
     }
     catch {
-        console.log('error');
+        console.log('Error removing tracks - maybe you hit the rate limit or your API key is invalid');
     }
 }
 
+async function deleteTracks(chunkedItemIds) {
+    chunkedItemIds.forEach(async (trackIds, index) => {
+        await new Promise(r => setTimeout(r, 250));
+        await deleteTrack(trackIds.toString());
+    });
+}
 
 async function getItems(nextUrl) {
     let tracks = [];
 
-    while(nextUrl) {
-        const response = await fetch(
-            nextUrl,
-            {
-                headers: {
-                    'Authorization': bearer
+    try {
+        while(nextUrl) {
+            const response = await fetch(
+                nextUrl,
+                {
+                    headers: {
+                        'Authorization': bearer
+                    }
                 }
+            );
+
+            if (response) {
+                const parsedResponse = await response.json();
+
+                tracks.push(...parsedResponse.items);
+                nextUrl = parsedResponse.next;
             }
-        );
-
-        if (response) {
-            const parsedResponse = await response.json();
-
-            tracks.push(...parsedResponse.items);
-            nextUrl = parsedResponse.next;
-
-            console.log(nextUrl);
-        }
-        else {
-            nextUrl = false;
-
-            console.log("Done!");
+            else {
+                nextUrl = false;
+            }
         }
     }
+    catch {
+        console.log('Error getting albums or tracks - maybe you hit the rate limit or your API key is invalid');
+    }
+
+
 
     return tracks;
 }
 
 async function init() {
+    console.log("Getting tracks...");
     const tracks = await getItems('https://api.spotify.com/v1/me/tracks?limit=50');
+    console.log(`Got ${tracks.length} tracks!`)
 
+    console.log("Getting albums...");
     const albums = await getItems('https://api.spotify.com/v1/me/albums?limit=50');
-
-    console.log("total tracks", tracks.length);
-    console.log("total albums", albums.length);
+    console.log(`Got ${albums.length} albums!`)
 
     const albumsWhoseTracksToRemove = albums.filter((album) => {
         const found = tracks.filter((track) => {
@@ -67,7 +77,7 @@ async function init() {
         });
 
         // If all tracks are in my library
-        if (found.length > album.album.total_tracks) {
+        if (found.length === album.album.total_tracks) {
             return true;
         }
         else {
@@ -75,7 +85,7 @@ async function init() {
         }
     }, []);
 
-    console.log("number of albums Whose Tracks ToRemove", albumsWhoseTracksToRemove.length);
+    console.log("Number of saved albums whose tracks are all saved:", albumsWhoseTracksToRemove.length);
 
     const tracksToRemove = tracks.filter((track) => {
         const found = albumsWhoseTracksToRemove.find((album) => {
@@ -85,19 +95,21 @@ async function init() {
         return found;
     })
 
-    console.log("Tracks to remove", tracksToRemove.length);
+    console.log("Tracks to remove:", tracksToRemove.length);
 
-    const tracksToRemoveIds = tracksToRemove.map((track) => {
-        return track.track.id;
-    })
+    if (tracksToRemove.length > 0){
+        console.log("Removing tracks...");
 
-    const chunkedTracksToRemoveIds = chunks(tracksToRemoveIds, 50);
+        const tracksToRemoveIds = tracksToRemove.map((track) => {
+            return track.track.id;
+        })
 
-    chunkedTracksToRemoveIds.forEach(async (trackIds, index) => {
-        console.log(`deleting tracks ${index} out of ${chunkedTracksToRemoveIds.length}`)
-        await new Promise(r => setTimeout(r, 250));
-        await deleteItem(trackIds.toString());
-    });
+        const chunkedTracksToRemoveIds = chunks(tracksToRemoveIds, 50);
+
+        await deleteTracks(chunkedTracksToRemoveIds);
+    }
+
+    console.log("Done!");
 }
 
 init();
