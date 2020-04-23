bea8971417ebb878fef66f2d8f618d693e1b64ae
Joe Duncko <JoeDuncko@users.noreply.github.com>
2020-04-23 19:49:24 -0400

    Install eslint, lint everything

--- src/Copyright.js
+++ src/Copyright.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import MuiLink from '@material-ui/core/Link';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import MuiLink from "@material-ui/core/Link";
 
 export default function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <MuiLink color="inherit" href="https://material-ui.com/">
         Your Website
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+            </MuiLink>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
 }
